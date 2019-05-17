f46791c56b02e2f2808d89f669dcdb6ed3aba49e
smprov22 <smprovence22@gmail.com>
2019-05-16 18:38:49 -0700

    final

--- liri.js
+++ liri.js
@@ -6,13 +6,14 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var fs = require("fs");
 var command = process.argv[2];
+var argument = process.argv.splice(3, process.argv.length - 1)
 
 if (command === `concert-this`) {
-    bandsInTown()
+    bandsInTown(argument)
 } else if (command === `spotify-this-song` ) {
-    spotifySong()
+    spotifySong(argument)
 } else if (command === `movie-this`) {
-    omdb()
+    omdb(argument)
 } else if (command === `do-what-it-says`) {
     doWhatItSays()
 }
@@ -20,9 +21,8 @@ if (command === `concert-this`) {
 
 //BANDS IN TOWN LIRI
 // `node liri.js concert-this <artist/band name here>`
-function bandsInTown() {
+function bandsInTown(artistInput) {
 // * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
-    var artistInput = process.argv.splice(3, process.argv.length - 1);
     var artist = artistInput.join("+");
     var artistFormat = artistInput.join(" ");
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
@@ -44,11 +44,11 @@ function bandsInTown() {
 }
 //SPOTIFY LIRI
 // * This will show the following information about the song in your terminal/bash window
-function spotifySong() {
-    var songName = process.argv.splice(3, process.argv.length - 1);
+function spotifySong(songName) {
 
-    if (songName === undefined) {
+    if (songName.length < 1) {
         songName = "The Sign";
+        
       }
 
     spotify.search({ type: 'track', query: songName }, function(err, data) {
@@ -75,16 +75,17 @@ function spotifySong() {
 }
 //OMBD LIRI
 // `node liri.js movie-this '<movie name here>'`
-function omdb() {
+function omdb(movieName) {
 
-    var movieName = process.argv.splice(3, process.argv.length - 1);
-    var onlyName = movieName.join("+");
-
-    var queryUrl = "http://www.omdbapi.com/?t=" + onlyName + "&y=&plot=short&apikey=trilogy";
-
-    if (movieName === undefined) {
-        movieName = "Mr Nobody";
+    if (movieName.length < 1) {
+        movieName = "Mr+Nobody";
       }
+
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    //    * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+   
     // This line is just to help us debug against the actual URL.
     // console.log(queryUrl);
     //      ```
@@ -100,22 +101,24 @@ function omdb() {
             console.log(response.data.Title + " stars: " + response.data.Actors);
         }
       );
-
-//    * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 }
+
 //DO WHAT IT SAYS LIRI
 // `node liri.js do-what-it-says`
-var doWhatItSays = function() {
+function doWhatItSays() {
     fs.readFile("random.txt", "utf8", function(error, data) {
-      console.log(data);
-  
-      var dataArr = data.split(",");
-  
+    //   console.log(data);
+    if (error) {
+        return console.log(error);
+      }
+    var dataArr = data.split(",");
 
+    if(dataArr[0] === "spotify-this-song") {
+        spotifySong(dataArr[1]);
+    } else if (dataArr[0] === "movie-this") {
+        omdb(dataArr[1]);
+    } else if (dataArr[0] === "concert-this") {
+        bandsInTown(dataArr[1]);
+    }
     });
   };
-//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-
-//      * Edit the text in random.txt to test out the feature for movie-this and concert-this.
